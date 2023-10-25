47191c46f194b3bbc3b0cb0652ae99d5f0b1eb6d
Kanyshai4 <144453937+Kanyshai4@users.noreply.github.com>
2023-10-25 09:49:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,101 @@
 
-#n=int(input())
-#F = [0] * (n + 1)
-#F[1] = 1
-#for i in range(2, n + 1):
-   # F[i] = (i%2==0) + F[i-1]
-#print(F[i])
 
-#def f(x):
-  #  if (x==0):
-        #return 0
-    #if (x==1):
-        #return 0
-    #if (x==2):
-        #return 2
+import seaborn as sns
+from matplotlib import pyplot as plt
 
-import matplotlib.pyplot as plt
-
-n = 100
-Price=[]
-for ii in range(0,101):
-    Price.append(ii)
-F = [0] * (n + 1)
-F[1] = 1
-itogst=0
-for i in range(2, n+1):
-    if(i%2==0):
-        if Price[i-1] < Price[i//2]:
-            itogst+=Price[i-1]
-        if Price[i-1] >= Price[i//2]:
-            itogst+=Price[i//2]
-    else:
-        itogst+=Price[i]
-print(itogst)
+df = sns.load_dataset("penguins")
+sns.pairplot(df, hue="species")
 
 
-for i in range(2, n+1):
-    if i -1 >= 1:
-        F[i] += F[i - 1]
-    if i % 2 == 0 and i // 2 >= 1:
-        F[i] += F[i // 2]
+stuffdict = {'couch_s':(300,75),
+             'couch_b':(500,80),
+             'bed':(400,100),
+             'closet':(200,50),
+             'bed_s':(200,40),
+             'desk':(200,70),
+             'table':(300,80),
+             'tv_table':(200,30),
+             'armchair':(100,30),
+             'bookshelf':(200,60),
+             'cabinet':(150,20),
+             'game_table':(150,30),
+             'hammock':(250,45),
+             'diner_table_with_chairs':(250,70),
+             'stools':(150,30),
+             'mirror':(100,20),
+             'instrument':(300,70),
+             'plant_1':(25,10),
+             'plant_2':(30,20),
+             'plant_3':(45,25),
+             'sideboard':(175,30),
+             'chest_of_drawers':(25,40),
+             'guest_bed':(250,40),
+             'standing_lamp':(20,30),
+             'garbage_can':(30,35),
+             'bar_with_stools':(200,40),
+             'bike_stand':(100,80),
+             'chest':(150,25),
+             'heater':(100,25)
+            }
+def get_area_and_value(stuffdict):
+    area = [stuffdict[item][0] for item in stuffdict]
+    value = [stuffdict[item][1] for item in stuffdict]
+    return area, value
+def get_memtable(stuffdict, A=2000):
+    area, value = get_area_and_value(stuffdict)
+    n = len(value)
+    V = [[0 for a in range(A + 1)] for i in range(n + 1)]
+    for i in range(n + 1):
+        for a in range(A + 1):
+            if i == 0 or a == 0:
+                V[i][a] = 0
+            elif area[i - 1] <= a:
+                V[i][a] = max(value[i - 1] + V[i - 1][a - area[i - 1]], V[i - 1][a])
 
-print("Количество способов достичь точки n из точки 1:", F[n])
+            else:
+                V[i][a] = V[i - 1][a]
+    return V, area, value
 
-# Визуализация на координатной прямой
-x = list(range(1, n+1))
-y = F[1:]
+def get_selected_items_list(stuffdict, A=2000):
+    V, area, value = get_memtable(stuffdict)
+    n = len(value)
+    res = V[n][A]
+    a = A
+    items_list = []
+    for i in range(n, 0, -1):
+        if res <= 0:
+            break
+        if res == V[i - 1][a]:
+            continue
+        else:
+            items_list.append((area[i - 1], value[i - 1]))
+            res -= value[i - 1]
+            a -= area[i - 1]
+    selected_stuff = []
+    for search in items_list:
+        for key, value in stuffdict.items():
+            if value == search:
+                selected_stuff.append(key)
 
-plt.plot(x, y)
-plt.xlabel('Координата')
-plt.ylabel('Количество способов')
-plt.title('Оптимальное решение задачи о количестве способов достичь точки n из точки 1')
-plt.show()
+    return selected_stuff
+stuff = get_selected_items_list(stuffdict)
+print(stuff)
+totarea = sum([stuffdict[item][0] for item in stuff])
+totvalue = sum([stuffdict[item][1] for item in stuff])
+
+print(totarea)
+print(totvalue)
+
+def plot_memtable(V, stuffdict):
+    plt.figure(figsize=(30,15))
+    item_list = list(stuffdict.keys())
+    item_list.insert(0, 'empty')
+    sns.heatmap(V, yticklabels=item_list)
+    plt.yticks(size=25)
+    plt.xlabel('Area', size=25)
+    plt.ylabel('Added item', size=25)
+    plt.title('Value for Area with Set of Items', size=30)
+    plt.show()
+
+V, area, value = get_memtable(stuffdict, A=2000)
+print(plot_memtable(V, stuffdict))
